12856cd0d8efdb8c7c408106f58d45b559b60e7f
SaraOsacar <saraosacar@gmail.com>
2023-09-03 19:05:10 +0200

    Add links projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import '../styles/Projects.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 
 const projects = [
@@ -8,120 +11,132 @@ const projects = [
     title: 'Rick & Morty',
     description: (
       <>
-        <div className="project-tools">HTML5 | SASS | REACT</div>
         <div className="project-title">
           Favorite character and series information search
         </div>
+        <div className="project-tools">HTML5 | SASS | REACT</div>
       </>
     ),
     imageUrl:
       'https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/series-television/11-cosas-que-no-sabias-de-rick-y-morty/137666502-1-esl-ES/11-cosas-que-no-sabias-de-Rick-y-Morty.jpg?crop=1xw:0.75xh;center,top&resize=1200:*',
     projectUrl: 'https://saraosacar.github.io/Rick-and-Morty-React-Project/',
+    projectUrlGithub:
+      'https://github.com/SaraOsacar/Rick-and-Morty-React-Project',
   },
   {
     id: 2,
     title: 'Task List',
     description: (
       <>
-        <div className="project-tools">HTML5 | SASS | REACT</div>
         <div className="project-title">
           Task list App for adding, modifying or deleting tasks
         </div>
+        <div className="project-tools">HTML5 | SASS | REACT</div>
       </>
     ),
     imageUrl:
       'https://img.freepik.com/fotos-premium/cuaderno-blanco-boligrafo-encima-mesa-escritorio-oficina-madera-computadora-portatil-vista-superior_340270-360.jpg?w=740',
     projectUrl: 'https://saraosacar.github.io/Task-List/',
+    projectUrlGithub: 'https://github.com/SaraOsacar/Task-List',
   },
   {
     id: 3,
     title: 'Weather Widget',
     description: (
       <>
-        <div className="project-tools">HTML5 | SASS | REACT</div>
         <div className="project-title">
           Weather forecast App in the city of your choice
         </div>
+        <div className="project-tools">HTML5 | SASS | REACT</div>
       </>
     ),
     imageUrl:
       'https://img.freepik.com/vector-gratis/solicitud-pronostico-tiempo_1284-34616.jpg',
     projectUrl: 'https://saraosacar.github.io/Weather-Widget//',
+    projectUrlGithub: 'https://github.com/SaraOsacar/Weather-Widget',
   },
   {
     id: 4,
     title: 'Friends',
     description: (
       <>
-        <div className="project-tools">HTML5 | SASS | REACT</div>
         <div className="project-title">
           Search engine for the most famous Friends quotes
         </div>
+        <div className="project-tools">HTML5 | SASS | REACT</div>
       </>
     ),
     imageUrl: 'https://www.magisnet.com/wp-content/uploads/2019/09/friends.jpg',
     projectUrl: 'https://saraosacar.github.io/Friends-React-Project/',
+    projectUrlGithub: 'https://github.com/SaraOsacar/Friends-React-Project',
   },
   {
     id: 5,
     title: 'Dice Bets',
     description: (
       <>
-        <div className="project-tools">HTML5 | CSS3 | JavaScript</div>
         <div className="project-title">
           Dice betting game where you can lose or double your bet
         </div>
+        <div className="project-tools">HTML5 | CSS3 | JavaScript</div>
       </>
     ),
     imageUrl:
       'https://www.rushbet.co/blog/wp-content/uploads/2022/04/craps-games.jpg',
     projectUrl:
       'https://saraosacar.github.io/Dados-y-apuestas-JavaScript-project/',
+    projectUrlGithub:
+      'https://github.com/SaraOsacar/Dados-y-apuestas-JavaScript-project',
   },
   {
     id: 6,
     title: 'Awesome Profile Cards',
     description: (
       <>
-        <div className="project-tools">HTML5 | SASS | JavaScript</div>
         <div className="project-title">
           Team project for generating personalized contact cards
         </div>
+        <div className="project-tools">HTML5 | SASS | JavaScript</div>
       </>
     ),
     imageUrl:
       'http://beta.adalab.es/project-promo-t-module-2-team-2/assets/images/logo.png',
     projectUrl: 'http://beta.adalab.es/project-promo-t-module-2-team-2/',
+    projectUrlGithub:
+      'https://github.com/SaraOsacar/Awesome-Profile-Cards-JavaScript-Team-Project',
   },
   {
     id: 7,
     title: 'Cleopatras Digital Empire',
     description: (
       <>
-        <div className="project-tools">HTML5 | CSS3 |</div>
         <div className="project-title">
           Team project to create company website template
         </div>
+        <div className="project-tools">HTML5 | CSS3 |</div>
       </>
     ),
     imageUrl:
       'https://cadenaser.com/resizer/hIo8_l1sKWpqCYGb29p2VJI-zH8=/768x576/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/prisaradio/QJY44JN2EJDBJOZ47ZRP5TAGSY.jpg',
     projectUrl: 'http://beta.adalab.es/project-promo-t-module-1-team-1/',
+    projectUrlGithub:
+      'https://github.com/SaraOsacar/Cleopatras-s-Digital-Empire-Team-Project',
   },
   {
     id: 8,
     title: 'Anonymous Proxy',
     description: (
       <>
-        <div className="project-tools">HTML5 | CSS3</div>
         <div className="project-title">
           Responsive web and styling created via a Zeplin design
         </div>
+        <div className="project-tools">HTML5 | CSS3</div>
       </>
     ),
     imageUrl:
       'https://queverenelmundo.com/guias/wp-content/uploads/Vistas-rockefeller-center-1.jpg',
     projectUrl: 'https://saraosacar.github.io/Anonymous-Proxy/',
+    projectUrlGithub: 'https://github.com/SaraOsacar/Anonymous-Proxy',
   },
 ];
 
@@ -138,13 +153,22 @@ const Projects = () => {
             <img src={project.imageUrl} alt={project.title} />
             <h2>{project.title}</h2>
             <p>{project.description}</p>
+            <div className="project-links-container">
             <a
               href={project.projectUrl}
               target="_blank"
               rel="noopener noreferrer"
             >
-              View Project
+              <FontAwesomeIcon icon={faGlobe} />
             </a>
+            <a
+              href={project.projectUrlGithub}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+            </div>
           </div>
         ))}
       </div>
